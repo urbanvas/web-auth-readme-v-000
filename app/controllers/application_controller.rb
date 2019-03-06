fec29b3eca65ce760daf7b526209878bc12ff945
Neil Seejoor <nvseejoor@gmail.com>
2019-03-06 22:44:13 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,7 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
-
+  before_action :authenticate_user
   private
 
   def authenticate_user
@@ -11,7 +11,7 @@ class ApplicationController < ActionController::Base
     foursquare_url = "https://foursquare.com/oauth2/authenticate?client_id=#{client_id}&response_type=code&redirect_uri=#{redirect_uri}"
     redirect_to foursquare_url unless logged_in?
   end
-  
+
   def logged_in?
     !!session[:token]
   end
